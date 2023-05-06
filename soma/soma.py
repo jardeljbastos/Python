c83cdbfe5fbd6d9b29aa0058a3a0f13af649f217
Jardel Bastos <84300151+realidademax@users.noreply.github.com>
2023-05-06 18:53:12 -0300

    Envio Git

---
+++ soma/soma.py
@@ -0,0 +1,4 @@
+valor1 = input("Digite um valor para somar: ")
+valor2 = input('Digite um segundo valor para somar: ')
+somatotal = int(valor1) + int(valor2)
+print(f'O valor da soma dos valores é: {somatotal}')
